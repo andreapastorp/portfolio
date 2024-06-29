5c34fa4ed6e4fb9dc32a5634a9e173680b0b63ec
Andrea <andreapastorp@gmail.com>
2024-06-29 13:34:00 +0200

    Add project links and template Gerrard project

--- public/app.py
+++ public/app.py
@@ -9,3 +9,19 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/projects/gerrard-street")
+def gerrard_street():
+    return render_template('projects/gerrard-street.html')
+
+@app.route("/projects/twill-by-maersk")
+def twill():
+    return render_template('projects/twill-by-maersk.html')
+
+@app.route("/projects/connecting-roommates")
+def connecting_roommates():
+    return render_template('projects/connecting-roommates.html')
+
+@app.route("/projects/interbank")
+def interbank():
+    return render_template('projects/interbank.html')
